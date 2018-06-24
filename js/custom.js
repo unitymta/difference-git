f881667d62fba9a6994882a5bfdff5c5209cc352
unitymta <tranducsang.mta@gmail.com>
2018-06-24 22:32:41 +0700

    update
    update responsive, fonts

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,52 @@ $('#btn-menu-mobile').click(function(){
 	$('#header-container .container').toggleClass('active');
 	$('.main-menu').toggleClass('active');
 });
+
+
+/* check mobile device */
+$(document).ready(function(){
+	$(window).resize(function(){
+	    var width = $(window).width();
+
+	    if (width <= 1199){
+	    	$('body').addClass('responsive_1199');
+	    }
+	    else{
+	    	$('body').removeClass('responsive_1199');
+	    }
+	    if (width <= 1250){
+	    	$('body').addClass('responsive_1250');
+	    }
+	    else{
+	    	$('body').removeClass('responsive_1250');
+	    }
+	    if (width <= 991){
+	    	$('body').addClass('responsive_991');
+	    }
+	    else{
+	    	$('body').removeClass('responsive_991');
+	    }
+	    if (width <= 767){
+	        $('body').addClass('responsive_767');
+	    }
+	    else{
+	        $('body').removeClass('responsive_767');
+	    }
+	    if (width <= 640){
+	        $('body').addClass('responsive_640');
+	    }
+	    else{
+	        $('body').removeClass('responsive_640');
+	    }
+	});
+});
+
+
+/* go to top */
+document.getElementById('go-to-top').addEventListener("click", function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
+
